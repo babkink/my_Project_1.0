5778506b5daa998010ef1275acc81d16c3b2ac67
babkink <babkink72@gmail.com>
2024-09-02 16:08:26 +0300

    Update my file.py

--- my file.py
+++ my file.py
@@ -8,4 +8,5 @@ print(2*2+2)
 print(2*(2+2))
 print(2*2+2==2*(2+2))
 #4th program
-print(int((float('123.456')-int(float('123.456')))*10))
+#print(int((float('123.456')-int(float('123.456')))*10))
+print(int(float(123.456)*10)%10)
