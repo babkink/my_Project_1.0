153f7f94060907b63d8f1730dace5b8c8247e604
babkink <babkink72@gmail.com>
2024-09-02 13:06:56 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,7 @@
+homework_number=12
+hours_spent=2
+course_name='Python'
+average_time_of_homework=hours_spent/homework_number
+print(average_time_of_homework, 'hours')
+print(average_time_of_homework*60,'min')
+print('Курс: ',course_name,', всего задач ',homework_number,', затрачено времени ',hours_spent, ' часа. Среднее время выполнения =',average_time_of_homework*60,' мин',sep='')
