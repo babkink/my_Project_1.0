b9ca75355009d40855022d09e9b33901e7effd85
babkink <babkink72@gmail.com>
2024-09-02 12:36:42 +0300

    Add files via upload

---
+++ my file 1.py
@@ -0,0 +1,8 @@
+example='АмбассадоР'
+print(example[0])
+print(example[-1])
+print(example[5::])
+print(example[::-1])
+print(example[::2])
+print(example[len(example)//2::])
+print(example[:len(example)//2:])
